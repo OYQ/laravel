d66b7be64d4127fcd9f4fdbe3cf7c20224835e67
OYQ <aquan1264@vip.qq.com>
2017-12-16 16:10:19 +0800

    修改登陆页

--- public/js/login/login.js
+++ public/js/login/login.js
@@ -1,28 +1,7 @@
-function run() {
-    // var image = document.getElementById('background');
-    // image.onload = function() {
-    //     var engine = new RainyDay({
-    //         image: this
-    //     });
-    //     engine.rain([ [1, 2, 8000] ]);
-    //     engine.rain([ [3, 3, 0.88], [5, 5, 0.9], [6, 2, 1] ], 100);
-    // };
-    // image.crossOrigin = 'anonymous';
-    // image.src = 'img/N7ETzFO.jpg';
-    var image = document.getElementById('background');
-    image.onload = function() {
-        var engine = new RainyDay({
-            image: this
-        });
-        engine.rain(
-            [
-                [1, 0, 20],         // add 20 drops of size 1...
-                [3, 3, 1]           // ... and 1 drop of size from 3 - 6 ...
-            ],
-            100);
-    };
-                    // ... every 100ms
-    image.crossOrigin = 'anonymous';
-
-    image.src = 'img/N7ETzFO.jpg';
-}
+$(function () {
+    $('input').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' // optional
+    });
+});
--- resources/views/admin/login/index.blade.php
+++ resources/views/admin/login/index.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,50 +6,74 @@
     <title>AdminLTE 2 | Log in</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-
+    <!-- Bootstrap 3.3.7 -->
+    <link rel="stylesheet" href="../../adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../../adminlte/bower_components/font-awesome/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="../../adminlte/bower_components/Ionicons/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../../adminlte/dist/css/AdminLTE.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="../../adminlte/plugins/iCheck/square/blue.css">
     <link rel="stylesheet" href="../../css/login/login.css">
 
-    <script src="../../js/login/rainyday.min.js"></script>
-    <script src="../../js/login/login.js"></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-
-
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body onload="run();" id="body">
-<img id="background" alt="background" src="" />
-<div id="container">
-    <div class="row">
-        <div class="col-xs-12">
-            <span class="label label-primary" id="span">大棚环境监测系统</span>
-
-            <form method="post" action="/" class="login">
-                {{csrf_field()}}
-                <p>
-                    <label for="login">登录名:</label>
-                    <input type="text" name="name" id="login" class="form-control" placeholder="名字">
-                </p>
-
-                <p>
-                    <label for="password">密码:</label>
-                    <input type="password" name="password" id="password" class="form-control" placeholder="密码">
-                </p>
-
-                <p class="login-submit">
-                    <button type="submit" class="login-button">Login</button>
-                </p>
-
-            </form>
+{{--login-page--}}
+<body class="hold-transition">
+    <div class="login-box" id="container">
+        <div class="login-logo">
+            <a href="/"><b>大棚环境检测系统</b></a>
         </div>
+        <!-- /.login-logo -->
+        <div class="login-box-body">
+            <p class="login-box-msg">请登录</p>
+
+            <form action="/" method="post">
+                {{csrf_field()}}
+                <div class="form-group has-feedback">
+                    <input type="text" name="name" id="login" class="form-control" placeholder="名字">
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <input type="password" name="password" id="password" class="form-control" placeholder="密码">
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                </div>
+                <div class="row">
+                    <!-- /.col -->
+                    <div class="col-xs-4 pull-right">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">登录</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+
+
+        </div>
+        <!-- /.login-box-body -->
     </div>
 
 
 
-</div>
+<!-- /.login-box -->
 
-
+<!-- jQuery 3 -->
+<script src="../../adminlte/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
 <script src="../../adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- iCheck -->
+<script src="../../adminlte/plugins/iCheck/icheck.min.js"></script>
 
+<script src="../../js/login/login.js"></script>
 
 </body>
-
 </html>
